4d57acf424da91d464c623887fdbde52808ffd46
CristianRicharte6 <102038261+CristianRicharte6@users.noreply.github.com>
2023-01-05 15:49:15 +0100

    Update testing.js

--- testing.js
+++ testing.js
@@ -2,7 +2,6 @@ const {
   time,
   loadFixture,
 } = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("Deployment", function () {
@@ -184,7 +183,7 @@ describe("Deployment", function () {
       );
 
       // BOOSTER - Set categories amount for BOOSTER & Create Boosters.
-      await ylNFT1155.setCategoryAmount("Soccer", "speed", 6);
+      await ylNFT1155.setCategoryAmount("Soccer", "speed", 5);
       console.log(
         "✅ Booster Category Soccer/speed created with a maximum of 5"
       );
@@ -248,7 +247,7 @@ describe("Deployment", function () {
         [1, 2, 3, 4, 5]
       );
 
-      const subVaultNFTs = await ylVault.vaultContract(Owner.address);
+      const subVaultNFTTransfer = await ylVault.vaultContract(Owner.address);
       const vaultNFTsCounter = await ylVault.NFTsCounter(
         Owner.address,
         "Tennis"
@@ -256,7 +255,7 @@ describe("Deployment", function () {
 
       console.log(
         `✅ ${vaultNFTsCounter}/5 NFTs sent from Owner address to New Subvault address:`,
-        subVaultNFTs
+        subVaultNFTTransfer
       );
 
       await expect(ylVault.storeNftFromWalletToVaultERC721(addr1.address, [1]))
@@ -276,126 +275,34 @@ describe("Deployment", function () {
         ylVault.storeNftFromWalletToVaultERC1155(Owner.address, 1, 1)
       ).to.be.reverted;
       console.log("🛡 Reverted if transfering Booster not created");
-      const subVaultBooster = await ylVault.vaultContract(Owner.address);
+
+      const subVaultBoosterTransfer = await ylVault.vaultContract(
+        Owner.address
+      );
+      expect(subVaultNFTTransfer).to.equal(subVaultBoosterTransfer);
       console.log(
         "✅ 5 Boosters sent from Owner address to Subvault address:",
-        subVaultBooster
+        subVaultBoosterTransfer
       );
+
+      // WITHDRAW ERC721 FROM VAULT AND PAY FEES
+
+      // WITHDRAW ERC721 FROM VAULT AND PAY FEES
     });
   });
 });
 
 // MISING ON THE TEST:
 /* 
-  Store in subvault ERC1155 (ERC115 RECEIVER needed?)
-  Withdraw from subvault.
-  Set category maximum amount.
+ -----SUBVAULT CONTRACT -----------------------
+  Withdraw from subvault NFTF & BOOSTERS. CHECK HOW TO GET ABI FROM SUBVAULT CONTRACT!!!!!.
+  Check balances after fees paid.
+  Burn Booster
+  ------- CONTEST GAME CONTRACT ------------------
   Play a game with subvault full.
   Pay tournament fees
-  try to start a game with subvault empty.
+  Check balances after fees paid.
+  try to start a game with subvault empty. (Expect error)
+  -------- AUCTION CONTRACT -------------
+  it may needs ERC1155 receiver.
 */
-
-// describe("Create transfer tokens, NFT & Boosters and create subvault", () {
-//   it("")
-// })
-// ----------------
-
-//   describe("Deployment", function () {
-//     it("Should set the right unlockTime", async function () {
-//       const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-//       expect(await lock.unlockTime()).to.equal(unlockTime);
-//     });
-
-//     it("Should set the right owner", async function () {
-//       const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-//       expect(await lock.owner()).to.equal(owner.address);
-//     });
-
-//     it("Should receive and store the funds to lock", async function () {
-//       const { lock, lockedAmount } = await loadFixture(
-//         deployOneYearLockFixture
-//       );
-
-//       expect(await ethers.provider.getBalance(lock.address)).to.equal(
-//         lockedAmount
-//       );
-//     });
-
-//     it("Should fail if the unlockTime is not in the future", async function () {
-//       // We don't use the fixture here because we want a different deployment
-//       const latestTime = await time.latest();
-//       const Lock = await ethers.getContractFactory("Lock");
-//       await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-//         "Unlock time should be in the future"
-//       );
-//     });
-//   });
-
-//   describe("Withdrawals", function () {
-//     describe("Validations", function () {
-//       it("Should revert with the right error if called too soon", async function () {
-//         const { lock } = await loadFixture(deployOneYearLockFixture);
-
-//         await expect(lock.withdraw()).to.be.revertedWith(
-//           "You can't withdraw yet"
-//         );
-//       });
-
-//       it("Should revert with the right error if called from another account", async function () {
-//         const { lock, unlockTime, otherAccount } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         // We can increase the time in Hardhat Network
-//         await time.increaseTo(unlockTime);
-
-//         // We use lock.connect() to send a transaction from another account
-//         await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-//           "You aren't the owner"
-//         );
-//       });
-
-//       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-//         const { lock, unlockTime } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         // Transactions are sent using the first signer by default
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw()).not.to.be.reverted;
-//       });
-//     });
-
-//     describe("Events", function () {
-//       it("Should emit an event on withdrawals", async function () {
-//         const { lock, unlockTime, lockedAmount } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw())
-//           .to.emit(lock, "Withdrawal")
-//           .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-//       });
-//     });
-
-//     describe("Transfers", function () {
-//       it("Should transfer the funds to the owner", async function () {
-//         const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-//           deployOneYearLockFixture
-//         );
-
-//         await time.increaseTo(unlockTime);
-
-//         await expect(lock.withdraw()).to.changeEtherBalances(
-//           [owner, lock],
-//           [lockedAmount, -lockedAmount]
-//         );
-//       });
-//     });
-//   });
-// });
