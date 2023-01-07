c06861e40e090734a57af3031e6e8610c98debd9
CristianRicharte6 <102038261+CristianRicharte6@users.noreply.github.com>
2023-01-07 12:40:48 +0100

    Update YLNFT1155.sol

--- contracts/YLNFT1155.sol
+++ contracts/YLNFT1155.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.9;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
